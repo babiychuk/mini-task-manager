984ad097619db64bbbf4c2a37d1d7d296de8dd63
Евгений Бабийчук <e.babiychuk@jr.local>
2019-11-01 16:52:15 +0200

    Updates

---
+++ precache-manifest.046a5a3de61ee63876353ce9c3e92db9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6e889a99b08bd13caef",
+    "url": "/mini-task-manager/static/js/runtime~main.9c125ac3.js"
+  },
+  {
+    "revision": "76e7a94ec7d88629f4a8",
+    "url": "/mini-task-manager/static/js/main.0b4bd8b0.chunk.js"
+  },
+  {
+    "revision": "7b9f41c871b61d3ee0db",
+    "url": "/mini-task-manager/static/js/2.0099f0e5.chunk.js"
+  },
+  {
+    "revision": "76e7a94ec7d88629f4a8",
+    "url": "/mini-task-manager/static/css/main.5c601199.chunk.css"
+  },
+  {
+    "revision": "02470617c5d3ab5d5762cc21226caa06",
+    "url": "/mini-task-manager/index.html"
+  }
+];
